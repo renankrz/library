00af89daf050936f195add30448307b3b1c2b448
renankrz <renankrz@gmail.com>
2023-11-30 22:13:13 -0300

    customize prompt

---
+++ src/main/java/dev/renankrz/library/CustomPromptProvider.java
@@ -0,0 +1,16 @@
+package dev.renankrz.library;
+
+import org.jline.utils.AttributedString;
+import org.jline.utils.AttributedStyle;
+import org.springframework.shell.jline.PromptProvider;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomPromptProvider implements PromptProvider {
+
+    public AttributedString getPrompt() {
+        return new AttributedString("library:> ",
+                AttributedStyle.DEFAULT.foreground(AttributedStyle.BLUE));
+    }
+
+}
