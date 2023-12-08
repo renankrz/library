8314ab21828c79773e4bbf5b46dbfd390d89c456
renankrz <renankrz@gmail.com>
2023-12-08 10:30:27 -0300

    change sequence name as a workaround to generating two sequences

--- src/main/java/dev/renankrz/library/package-info.java
+++ src/main/java/dev/renankrz/library/package-info.java
@@ -1,5 +1,5 @@
 @org.hibernate.annotations.GenericGenerator(name = "ID_GENERATOR", strategy = "enhanced-sequence", parameters = {
-        @org.hibernate.annotations.Parameter(name = "sequence_name", value = "LIBRARY_SEQUENCE"),
+        @org.hibernate.annotations.Parameter(name = "sequence_name", value = "ID_GENERATOR"),
         @org.hibernate.annotations.Parameter(name = "initial_value", value = "1000")
 })
 
