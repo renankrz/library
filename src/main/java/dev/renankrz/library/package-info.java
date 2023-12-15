efc89d66985dc615bf8c7098ab4498f7a97a02ef
renankrz <renankrz@gmail.com>
2023-12-15 08:36:06 -0300

    start id sequence at 100000

--- src/main/java/dev/renankrz/library/package-info.java
+++ src/main/java/dev/renankrz/library/package-info.java
@@ -1,6 +1,6 @@
 @org.hibernate.annotations.GenericGenerator(name = "ID_GENERATOR", strategy = "enhanced-sequence", parameters = {
         @org.hibernate.annotations.Parameter(name = "sequence_name", value = "ID_GENERATOR"),
-        @org.hibernate.annotations.Parameter(name = "initial_value", value = "10000")
+        @org.hibernate.annotations.Parameter(name = "initial_value", value = "100000")
 })
 
 package dev.renankrz.library;
